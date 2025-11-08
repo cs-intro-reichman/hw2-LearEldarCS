f6527a726c210453a9000c6ab767091c87692c2d
Lear Eldar <lear.eldar@post.runi.ac.il>
2025-11-08 22:45:16 +0200

    Made the code for Cheers

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,28 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	
+                String wordInput = args[0];
+                int numTimes = Integer.parseInt(args[1]);
+                
+                for (int i = 0; i < wordInput.length(); i++) {
+                        
+                        char singleWord = wordInput.charAt(i); //each loop singleWorld would have a different letter
+                        String specialWord = "AEFHILMNORSX"; //all our special cases, that will be checked each loop
+
+                        if (specialWord.indexOf(singleWord) >= 0) {
+                                System.out.println("Give me an " + singleWord + ": " + singleWord + "!");
+                        } else {
+                                 System.out.println("Give me a  " + singleWord + ": " + singleWord + "!");
+                        }
+                }
+
+                System.out.println("What does that spell?");
+
+                for (int j = 0; j < numTimes; j++) {
+                        System.out.println(wordInput + "!!!");
+                }
+
+
         }
 }
