9c6c7142f062dee33bd8f4568fc9d694f37b9375
Lear Eldar <lear.eldar@post.runi.ac.il>
2025-11-10 15:14:00 +0200

    Made the code for CalcPi

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,23 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    
+		int numTerm = Integer.parseInt(args[0]);
+		
+		double pi = Math.PI;
+
+		double finalAprox = 0;
+
+		for (int a = 0; a < numTerm; a++) {
+			
+			double terms = 4*(Math.pow(-1,a))/(2*a +1); //the general form times 4
+			
+			finalAprox = finalAprox + terms;
+
+		}
+
+		System.out.println("pi according to Java: " + pi);
+		System.out.println("pi, approximated: " + finalAprox);
+
 	}
 }
