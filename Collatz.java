06d6eac92a1f2836ff844d12d5528a681bb77d67
Lear Eldar <lear.eldar@post.runi.ac.il>
2025-11-11 22:56:48 +0200

    Made the code for Collatz

--- Collatz.java
+++ Collatz.java
@@ -28,7 +28,7 @@ public class Collatz {
 						System.out.println();
 				}
 	 		} else if (mode.equals("c")) {
-				System.out.println("Every one of the first " + N + " hailstone sequences reached 1");;
+				System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");;
 			}
 		
 		
